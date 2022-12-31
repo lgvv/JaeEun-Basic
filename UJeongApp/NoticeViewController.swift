3ed12b3b5a337248135c7feffc5b3f8d22587919
lgvv <lgw9898@naver.com>
2022-12-31 13:44:34 +0900

    [추가] 노티스 구현 브랜치를 추가합니다.

--- UJeongApp/NoticeViewController.swift
+++ UJeongApp/NoticeViewController.swift
@@ -7,15 +7,50 @@
 
 import UIKit
 import RxSwift
+import RxCocoa
+import SnapKit
 
 final class NoticeViewController: UIViewController {
-
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        view.backgroundColor = .yellow
     }
 }
 
-extension NoticeViewController {
-    
+final class NoticeViewModel: ViewModelType {
+    func transform(input: Input) -> Output {
+        
+        return Output(notices: <#T##Driver<SearchInfomation>#>)
+    }
 }
+
+extension NoticeViewModel {
+    struct Input {
+        let searchTrigger: Driver<Void>
+    }
+    
+    struct Output {
+        let notices: Driver<SearchInfomation>
+    }
+}
+
+
+//final class NoticeViewModel: ViewModelType {
+//    typealias Input = <#type#>
+//
+//    typealias Output = <#type#>
+//
+//    let input = PublishSubject<String>()
+//
+//    let output: Driver<SearchInfomation>
+//
+//    init(apiClient: APIClient = APIClient()) {
+//        self.output = apiClient.searchWithRx()
+//            .asDriver(onErrorJustReturn: .init(totalCount: nil, incompleteResults: nil, items: []))
+//
+//    }
+//}
+//
+//extension NoticeViewController {
+//
+//}
--- UJeongAppTests/UJeongAppTests.swift
+++ UJeongAppTests/UJeongAppTests.swift
@@ -8,29 +8,47 @@
 import XCTest
 @testable import UJeongApp
 
-final class UJeongAppTests: XCTestCase {
+final class APIClientTests: XCTestCase {
 
-    override func setUpWithError() throws {
-        // Put setup code here. This method is called before the invocation of each test method in the class.
+    var apiClient: APIClient? // sut
+
+    override func setUp() {
+        super.setUp()
+        
+        apiClient = APIClient()
     }
 
-    override func tearDownWithError() throws {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    override func tearDown() {
+        super.tearDown()
+        
+        apiClient = nil
     }
-
-    func testExample() throws {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
-        // Any test you write for XCTest can be annotated as throws and async.
-        // Mark your test throws to produce an unexpected failure when your test encounters an uncaught error.
-        // Mark your test async to allow awaiting for asynchronous code to complete. Check the results with assertions afterwards.
-    }
-
-    func testPerformanceExample() throws {
-        // This is an example of a performance test case.
-        self.measure {
-            // Put the code you want to measure the time of here.
+    
+    func test_search() throws {
+        
+        let expectation = XCTestExpectation()
+        expectation.expectedFulfillmentCount = 1
+        
+        let q = "RxSwift"
+        let page = 1
+        
+        apiClient?.search(q: q, page: page) { result in
+            switch result {
+            case .success(let repoOrNil):
+                
+                
+                XCTAssert(true)
+                
+            case .failure(let error):
+                if let error = error as? APIClient.APIError {
+                    XCTFail("정의한 에러" + error.localizedDescription)
+                } else {
+                    XCTFail(error.localizedDescription)
+                }
+            }
+            expectation.fulfill()
         }
+        
+        wait(for: [expectation], timeout: 30)
     }
-
 }
