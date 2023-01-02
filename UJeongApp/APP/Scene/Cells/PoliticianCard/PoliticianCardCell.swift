1612144a11870712c15bf0f0bbde2d3d69f15ebf
lgvv <lgw9898@naver.com>
2023-01-03 03:45:06 +0900

    [추가] 카드셀 구현 완료

--- UJeongApp/APP/Scene/Cells/PoliticianCard/PoliticianCardCell.swift
+++ UJeongApp/APP/Scene/Cells/PoliticianCard/PoliticianCardCell.swift
@@ -15,7 +15,10 @@ final class PoliticianCardCell: BaseCollectionViewCell {
     // MARK: - Binding
     
     func bind(_ viewModel: PoliticianCardCellViewModel) {
-        
+        self.thumbnailImageView.image = UIImage()
+        self.nameLabel.text = viewModel.politician.name
+        self.partyLabel.text = viewModel.politician.party
+        self.descriptionLabel.text = ""
     }
     
     override func initialize() {
@@ -29,11 +32,15 @@ final class PoliticianCardCell: BaseCollectionViewCell {
                         .aspectRatio(1)
                 }
                 
-            flex.addItem(nameLabel)
+            flex.addItem()
+                .direction(.row)
+                .define { flex in
+                    flex.addItem(nameLabel)
+                    flex.addItem(partyLabel)
+                }
                 .marginLeft(10%)
             
             flex.addItem()
-                .alignItems(.center)
                 .define { flex in
                     flex.addItem(nameLabel).grow(1)
                     flex.addItem(descriptionLabel).grow(4)
@@ -55,17 +62,14 @@ final class PoliticianCardCell: BaseCollectionViewCell {
     
     let thumbnailImageView = UIImageView().config {
         $0.backgroundColor = .blue.withAlphaComponent(0.2)
-    }
+    } // 이미지
     
-    let nameLabel = UILabel().config {
-        $0.font = .systemFont(ofSize: 30, weight: .semibold)
-    }
+    let nameLabel = UILabel() // 이름
+    let partyLabel = UILabel() // 당
+    
     let descriptionLabel = UILabel().config {
         $0.numberOfLines = 0
         $0.font = .systemFont(ofSize: 15)
-    }
+    } // 강점 3줄
 }
 
-final class PoliticianCardCellViewModel {
-    
-}
---
+++ UJeongApp/APP/Scene/Cells/PoliticianCard/PoliticianCardCellViewModel.swift
@@ -0,0 +1,16 @@
+//
+//  PoliticianCardCellViewModel.swift
+//  UJeongApp
+//
+//  Created by Hamlit Jason on 2023/01/03.
+//
+
+import Foundation
+
+final class PoliticianCardCellViewModel {
+    let politician: Politician
+    
+    init(with politician: Politician) {
+        self.politician = politician
+    }
+}
