a6aee5c1c10d02ea0065495063ddcd8dd32e07d5
lgvv <lgw9898@naver.com>
2023-01-03 04:00:21 +0900

    [추가] 섹션추가

--- UJeongApp/APP/Scene/Politician/PoliticianSection.swift
+++ UJeongApp/APP/Scene/Politician/PoliticianSection.swift
@@ -8,4 +8,42 @@
 import Foundation
 import RxDataSources
 
+enum PoliticianSectionItem: Equatable, IdentifiableType {
+    var identity: some Hashable { UUID().uuidString }
+    
+    case politicianCardCell(PoliticianCardCellViewModel)
+    
+    static func == (lhs: PoliticianSectionItem, rhs: PoliticianSectionItem) -> Bool {
+        lhs.identity == rhs.identity
+    }
+}
 
+enum PoliticianSection {
+    case politicianCardCell([PoliticianSectionItem])
+}
+
+extension PoliticianSection: SectionModelType {
+    typealias Item = PoliticianSectionItem
+    
+    var items: [PoliticianSectionItem] {
+        switch self {
+        case .politicianCardCell(let items): return items
+        }
+    }
+    
+    init(original: PoliticianSection, items: [PoliticianSectionItem]) {
+        switch original {
+        case .politicianCardCell: self = .politicianCardCell(items)
+        }
+    }
+    
+    func convertToSection(from items: [PoliticianSection]) {
+        let cardItems: [PoliticianSectionItem] = items.map { politician in
+            let viewModel = PoliticianCardCellViewModel(with: politician)
+            return PoliticianSectionItem.politicianCardCell(viewModel)
+        }
+        
+        let cardSection: PoliticianSection = PoliticianSection.politicianCardCell(cardItems)
+        return [cardSection]
+    }
+}
