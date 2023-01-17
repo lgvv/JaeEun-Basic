c70d6dce2a328a63579376f39dc4ebea34048f91
lgvv <lgw9898@naver.com>
2023-01-17 20:13:01 +0900

    [추가] 구청장 json stringset

--- UJeongApp/SDKModule/FirebaseModule/FirebaseDelegate.swift
+++ UJeongApp/SDKModule/FirebaseModule/FirebaseDelegate.swift
@@ -6,7 +6,28 @@
 //
 
 import Foundation
+import FirebaseDatabase
 
-public class FirebaseDelegate {
+public class FirebaseDatabaseService {
+    private var ref: DatabaseReference!
+
+    init() {
+        ref = Database.database().reference()
+    }
     
+    // MARK: - Write
+    func write() {
+        print("나는 불립니다요")
+        self.ref.child("politician").setValue(["username":"강감찬."])
+    }
+    
+    // MARK: - Read
+    
+    enum ReadType {
+        case all
+    }
+    
+    private func read(forType: ReadType) {
+        
+    }
 }
