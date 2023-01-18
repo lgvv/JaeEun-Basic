78fe732690a817ceb3300e2682ddf417c70c5810
lgvv <lgw9898@naver.com>
2023-01-18 16:35:10 +0900

    [수정] 파싱 제네릭 되는거 확인

--- UJeongApp/SDKModule/FirebaseModule/FirebaseDelegate.swift
+++ UJeongApp/SDKModule/FirebaseModule/FirebaseDelegate.swift
@@ -10,7 +10,7 @@ import FirebaseDatabase
 
 public class FirebaseDatabaseService {
     private var ref: DatabaseReference!
-
+    
     init() {
         ref = Database.database().reference()
     }
@@ -18,7 +18,10 @@ public class FirebaseDatabaseService {
     // MARK: - Write
     func write() {
         print("나는 불립니다요")
-        self.ref.child("politician").setValue(["username":"강감찬."])
+//        self.ref.child("politician").setValue(["username":"강감찬."])
+        let helper = jsonHelper()
+        let data = helper.loadLocalFile()
+        helper.parsejson(Welcome.self, data: data)
     }
     
     // MARK: - Read
@@ -30,4 +33,72 @@ public class FirebaseDatabaseService {
     private func read(forType: ReadType) {
         
     }
+}
+
+public class jsonHelper {
+    func loadLocalFile() -> Data? {
+        // 1. 로컬 파일 로드하기
+        let fileName: String = "MayorOfSeoulGu"
+        let extensionType: String = "json"
+        
+        guard let fileLoaction = Bundle.main.url(
+            forResource: fileName,
+            withExtension: extensionType
+        ) else {
+            print("AAAA")
+            return nil
+        }
+        
+        do {
+            let data = try Data(contentsOf: fileLoaction)
+            return data
+        } catch {
+            print("여기서잡힘")
+            return nil
+        }
+    }
+    
+    func parsejson<T:Codable>(_ type: T.Type, data: Data?) {
+        guard let data = data else {
+            print("out")
+            return
+        }
+        
+        do {
+            let result = try JSONDecoder().decode(T.self, from: data)
+            print(" result \(result)")
+            
+        } catch {
+            print("ERROR")
+        }
+        
+        print(" 나는요")
+        
+    }
+}
+
+struct Welcome: Codable {
+    let mayor: Mayor
+}
+
+// MARK: - Mayor
+struct Mayor: Codable {
+    let seoul: [Busan]
+}
+
+// MARK: - Busan
+struct Busan: Codable {
+    let gu, name: String
+    let party: Party
+    let imageURL: String
+
+    enum CodingKeys: String, CodingKey {
+        case gu, name, party
+        case imageURL = "image_url"
+    }
+}
+
+enum Party: String, Codable {
+    case 국민의힘 = "국민의힘"
+    case 더불어민주당 = "더불어민주당"
 }
